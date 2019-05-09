f796bbf3308cc1c5be03e2750850d7ee4ae35638
Yan Li <muzifeng1021@yahoo.com>
2019-05-09 14:06:09 +1000

    webserver sample files

---
+++ webserver/map.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+
+from flask import Flask,render_template,url_for
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_map():
+    return render_template('melbourne.html')
+
+@app.route('/linechart')
+def my_linechart():
+    return render_template('linechart.html')
+
+if __name__ == "__main__":
+    #运行项目
+    #app.run(debug = True）
+    app.run(use_reloader=True)
+
