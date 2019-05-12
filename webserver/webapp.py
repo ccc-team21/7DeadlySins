e76f68e13eb542afc84a8cf9aed2cea6958fbfc5
Yan Li <muzifeng1021@yahoo.com>
2019-05-12 23:42:12 +1000

    so far the latest version

--- webserver/webapp.py
+++ webserver/webapp.py
@@ -1,79 +1,107 @@
-from flask import Flask, render_template
+'''
+Team 21 Sentiment Analysis
+City: Melbourne
+Team Members:
+    Anupa Alex – 1016435
+    Luiz Fernando Franco - 1019613
+    Suraj Kumar Thakur - 999886
+    Waneya Iqbal - 919750
+    Yan Li – 984120
+'''
+
+from flask import Flask, render_template, jsonify
 import json
 import couchdb
 
 app = Flask(__name__)
 
 
-def getdoc(database_name):
-    couch = couchdb.Server('http://Yanli:muzifeng1021@127.0.0.1:5984')
+def getdb(database_name):
+    couch = couchdb.Server('http://admin:admin@172.26.38.106:5984')
     db = couch[database_name]
-    doc = db['0d99472d9fd2d79461528599f304bccb']
-    return doc
+    return db
 
 
 def querydb(database_name):
-    doc = getdoc(database_name)
+    db = getdb(database_name)
     lis = []
-    for i in range(122):
-        i += 1
-        order = str(i)
+    for each in db:
+        doc = db[each]
         content = {}
-        if doc[order]['name'] == 'mckinnon':
+        if doc['name'] == 'mckinnon':
             content['name'] = 'McKinnon'
         else:
-            content['name'] = doc[order]['name'].title()
-        content['value'] = doc[order]['count']
+            content['name'] = doc['name'].title()
+        content['value'] = doc['score']
         lis.append(content)
     return lis
 
 
 def getdata(database_name, value):
-    doc = querydb(database_name)
+    lis = querydb(database_name)
     data_list = []
     if value == "value":
-        for each in doc:
+        for each in lis:
             data_list.append(each["value"])
     else:
-        for each in doc:
+        for each in lis:
             data_list.append(each["name"])
     return data_list
 
 
 @app.route('/')
 def my_map():
-    twitterdata = querydb("twitter")
-    aurindata = [
-        {'name': 'Cremorne', 'value': 100},
-        {'name': 'Caulfield', 'value': 20},
-        {'name': 'Carnegie', 'value': 50},
-        {'name': 'Kingsville', 'value': 60}]
+    aurindata = querydb("aurin_uni")
+    lustdata = [
+            {'name': 'Cremorne', 'value': 0.3},
+            {'name': 'Caulfield', 'value': 0.6},
+            {'name': 'Carnegie', 'value': 0.55},
+            {'name': 'Kingsville', 'value': 0.55}]
 
-    instagramdata = [
-        {'name': 'Cremorne', 'value': 200},
-        {'name': 'Caulfield', 'value': 300},
-        {'name': 'Carnegie', 'value': 500},
-        {'name': 'Kingsville', 'value': 700}]
+    pridedata = [
+        {'name': 'Cremorne', 'value': 0.5},
+        {'name': 'Caulfield', 'value': 0.3},
+        {'name': 'Carnegie', 'value': 0.44},
+        {'name': 'Kingsville', 'value': 0.8}]
 
-    return render_template('map.html', twitterdata=json.dumps(twitterdata), aurindata=json.dumps(aurindata),
-                           instagramdata=json.dumps(instagramdata))
+    return render_template('map.html', aurindata=json.dumps(aurindata), lustdata=json.dumps(lustdata),
+                           pridedata=json.dumps(pridedata))
 
 
 @app.route('/linechart')
 def my_linechart():
 
-    twitterdata = getdata("twitter", "value")
+    aurindata = getdata("aurin_uni", "value")
 
-    aurindata = [100, 20, 50, 60]
-    instagramdata = [20, 100, 300, 30]
 
-    xlabel = getdata("twitter", "name")
+    lustdata = [0.3, 0.6, 0.55, 0.55]
 
-    return render_template('linechart.html', twitterdata=twitterdata, aurindata=aurindata,
-                           instagramdata=instagramdata, xlabel=xlabel)
+    pridedata = [0.5, 0.3, 0.44, 0.8]
+
+    xlabel = getdata("aurin_uni", "name")
+
+    return render_template('linechart.html', aurindata=aurindata, lustdata=lustdata,
+                           pridedata=pridedata, xlabel=xlabel)
+
+
+@app.route('/data')
+def my_data():
+    meta_data ={}
+    meta_data["aurin data"] = querydb("aurin_uni")
+    meta_data["twitter_data"] = [
+            {'name': 'Cremorne', 'value': 0.3},
+            {'name': 'Caulfield', 'value': 0.6},
+            {'name': 'Carnegie', 'value': 0.55},
+            {'name': 'Kingsville', 'value': 0.55}]
+    meta_data["instagram data"] = [
+        {'name': 'Cremorne', 'value': 0.5},
+        {'name': 'Caulfield', 'value': 0.3},
+        {'name': 'Carnegie', 'value': 0.44},
+        {'name': 'Kingsville', 'value': 0.8}]
+    return jsonify(meta_data)
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
-    #app.run(use_reloader=True)
+    app.run(host='0.0.0.0', port=5000)
+
 
