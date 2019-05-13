f5f9535d4f40543fb7a4e593c0609c8a5b57af11
Yan Li <muzifeng1021@yahoo.com>
2019-05-14 01:19:10 +1000

    Please remember to install flask_restful

--- webserver/webapp.py
+++ webserver/webapp.py
@@ -12,8 +12,10 @@ Team Members:
 from flask import Flask, render_template, jsonify
 import json
 import couchdb
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
+api = Api(app)
 
 
 def getdb(database_name):
@@ -25,6 +27,7 @@ def getdb(database_name):
 def querydb(database_name):
     db = getdb(database_name)
     lis = []
+    newlist = []
     for each in db:
         doc = db[each]
         content = {}
@@ -34,7 +37,8 @@ def querydb(database_name):
             content['name'] = doc['name'].title()
         content['value'] = doc['score']
         lis.append(content)
-    return lis
+        newlist = sorted(lis, key=lambda k: k['name'])
+    return newlist
 
 
 def getdata(database_name, value):
@@ -51,18 +55,9 @@ def getdata(database_name, value):
 
 @app.route('/')
 def my_map():
-    aurindata = querydb("aurin_uni")
-    lustdata = [
-            {'name': 'Cremorne', 'value': 0.3},
-            {'name': 'Caulfield', 'value': 0.6},
-            {'name': 'Carnegie', 'value': 0.55},
-            {'name': 'Kingsville', 'value': 0.55}]
-
-    pridedata = [
-        {'name': 'Cremorne', 'value': 0.5},
-        {'name': 'Caulfield', 'value': 0.3},
-        {'name': 'Carnegie', 'value': 0.44},
-        {'name': 'Kingsville', 'value': 0.8}]
+    aurindata = querydb("aurin_richness")
+    lustdata = querydb("lust_web")
+    pridedata = querydb("pride_web")
 
     return render_template('map.html', aurindata=json.dumps(aurindata), lustdata=json.dumps(lustdata),
                            pridedata=json.dumps(pridedata))
@@ -71,37 +66,49 @@ def my_map():
 @app.route('/linechart')
 def my_linechart():
 
-    aurindata = getdata("aurin_uni", "value")
+    aurindata = getdata("aurin_richness", "value")
 
+    lustdata = getdata("lust_web", "value")
 
-    lustdata = [0.3, 0.6, 0.55, 0.55]
+    pridedata = getdata("pride_web", "value")
 
-    pridedata = [0.5, 0.3, 0.44, 0.8]
-
-    xlabel = getdata("aurin_uni", "name")
+    xlabel = getdata("aurin_richness", "name")
 
     return render_template('linechart.html', aurindata=aurindata, lustdata=lustdata,
                            pridedata=pridedata, xlabel=xlabel)
 
 
-@app.route('/data')
-def my_data():
-    meta_data ={}
-    meta_data["aurin data"] = querydb("aurin_uni")
-    meta_data["twitter_data"] = [
-            {'name': 'Cremorne', 'value': 0.3},
-            {'name': 'Caulfield', 'value': 0.6},
-            {'name': 'Carnegie', 'value': 0.55},
-            {'name': 'Kingsville', 'value': 0.55}]
-    meta_data["instagram data"] = [
-        {'name': 'Cremorne', 'value': 0.5},
-        {'name': 'Caulfield', 'value': 0.3},
-        {'name': 'Carnegie', 'value': 0.44},
-        {'name': 'Kingsville', 'value': 0.8}]
-    return jsonify(meta_data)
+class Datasource(Resource):
+    def get(self, name):
+        meta_data = []
+        aurin = {}
+        aurin["name"] = "richness"
+        aurin["data"] = querydb("aurin_richness")
+        meta_data.append(aurin)
+
+        lustdata = {}
+        lustdata["name"] = "lust"
+        lustdata["data"] = querydb("lust_web")
+        meta_data.append(lustdata)
+
+        pridedata = {}
+        pridedata["name"] = "pride"
+        pridedata["data"] = querydb("pride_web")
+        meta_data.append(pridedata)
+        for data in meta_data:
+            if name == data["name"]:
+                return data, 200
+        return "Data not found", 404
+
+
+api.add_resource(Datasource, "/data/<string:name>")
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    #app.run(host='0.0.0.0', port=5000)
+    app.run(debug = True)
+
+
+
 
 
